f36229d0877cd111ad6e941524682614c2434d67
Lenovo YOGA <aigerimsamidinovaa@gmail.com>
2019-05-09 15:27:57 +0600

    .

--- src/main/java/com/example/projectXX/service/QuestionServiceImpl.java
+++ src/main/java/com/example/projectXX/service/QuestionServiceImpl.java
@@ -1,4 +1,41 @@
 package com.example.projectXX.service;
 
-public class QuestionServiceImpl {
+import com.example.projectXX.model.Question;
+import com.example.projectXX.repository.QuestionRep;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class QuestionServiceImpl implements CrudService<Question> {
+    @Autowired
+    private QuestionRep questionRep;
+    @Override
+    public List<Question> getAll() {
+        return questionRep.findAll();
+    }
+
+    @Override
+    public Question findById(Long id) {
+        return findById(id);
+    }
+
+    @Override
+    public Question save(Question question) {
+        return questionRep.save(question);
+    }
+
+    @Override
+    public Question update(Question question) {
+        return questionRep.save(question);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        questionRep.deleteById(id);
+    }
+
+    @Override
+    public void deleteAll() {
+questionRep.deleteAll();
+    }
 }
