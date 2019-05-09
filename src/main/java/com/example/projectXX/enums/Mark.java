ecefb6eb751865ca34db735b2595f40e1edb4cc8
Lenovo YOGA <aigerimsamidinovaa@gmail.com>
2019-05-09 14:40:22 +0600

    added:  Mark

--- src/main/java/com/example/projectXX/enums/Mark.java
+++ src/main/java/com/example/projectXX/enums/Mark.java
@@ -1,4 +1,9 @@
 package com.example.projectXX.enums;
 
 public enum Mark {
+    LIKE,
+    DISLIKE;
+
+    Mark() {
+    }
 }
--- src/main/java/com/example/projectXX/service/QuestionServiceImpl.java
+++ src/main/java/com/example/projectXX/service/QuestionServiceImpl.java
@@ -1,4 +1,4 @@
 package com.example.projectXX.service;
 
-public class ItemServiceImpl {
+public class QuestionServiceImpl {
 }
